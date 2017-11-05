0aa4445b7efb5c11b42a1812e3aab81a6088ddc4
Tom Turner <tom.turner2@googlemail.com>
2017-11-05 20:15:10 +0000

    First compute practice shader
    +Created simple compute shader that lerps red using the sine wave
    +Created texture loading script for red sine compute shader
    +Created folder structure for practice compute shaders
    +Created test scene with UI to display the target texture

---
+++ Assets/Compute Practice/Behaviours/SineRedLoader.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class TexUpdateEvent : UnityEvent<RenderTexture>  {};
+
+public class SineRedLoader : MonoBehaviour
+{
+    [SerializeField] ComputeShader compute_shader;
+    [SerializeField] TexUpdateEvent tex_update;
+
+    private int kernel_handle;
+    private RenderTexture tex;
+    private Renderer rend;
+
+
+    private void Start ()
+	{
+	    kernel_handle = compute_shader.FindKernel("CSMain");
+        CreateRenderTexture();
+        AssignTexture();
+        UpdateTexture();
+	}
+
+
+    private void CreateRenderTexture()
+    {
+        tex = new RenderTexture(256, 256, 24);
+        tex.enableRandomWrite = true;//must be set before creation
+        tex.Create();
+
+        rend = GetComponent<Renderer>();
+        rend.material.SetTexture("_MainTex", tex);
+    }
+
+
+    private void AssignTexture()
+    {
+        compute_shader.SetTexture(kernel_handle, "Result", tex);
+    }
+
+
+    private void UpdateTexture()
+    {
+        compute_shader.SetFloat("dt", Time.time);
+        compute_shader.Dispatch(kernel_handle, 256/8, 256/8, 1);
+        rend.material.SetTexture("_MainTex", tex);
+        tex_update.Invoke(tex);
+    }
+
+
+    private void Update()
+    {
+        UpdateTexture();
+    }
+
+}
